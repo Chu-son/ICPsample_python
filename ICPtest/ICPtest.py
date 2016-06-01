eeca72c24e35d7077db55e4c0401312d0d074ff0
KosukeNakamura <s15mh133@mail.saitama-u.ac.jp>
2016-06-01 21:58:15 +0900

    update
    勾配的なのを考慮
    精度向上

--- ICPtest/ICPtest.py
+++ ICPtest/ICPtest.py
@@ -9,10 +9,11 @@ import os
 import random
 from PIL import Image
 
-DIRPATH = u"C:\\Users\\user\\Documents\\なかむら\\つくばチャレンジ2015\\測定データ\\20160526230040\\"
-#DIRPATH = u"C:\\Users\\user\\Documents\\なかむら\\つくばチャレンジ2015\\測定データ\\20160526162818\\"
-DIRPATH = "C:\\Users\\KOSUKE\\Source\\Repos\\ICPsample_python\\20160526230040\\"
+DIRPATH = "C:\\Users\\user\\Documents\\なかむら\\つくばチャレンジ2015\\測定データ\\20160526230040\\"
+#DIRPATH = "C:\\Users\\user\\Documents\\なかむら\\つくばチャレンジ2015\\測定データ\\20160526162818\\"
+#DIRPATH = "C:\\Users\\KOSUKE\\Source\\Repos\\ICPsample_python\\20160526230040\\"
 
+#PCDファイルを用いないサンプル
 def ICPsample():
 
     #Simulation Parameters
@@ -65,12 +66,11 @@ def ICPsample():
     plt.legend(loc = "upper right")
     plt.show()
 
-def ICPMatching(data1, data2 , boundaryList = [[],[]]):
-    # ICPアルゴリズムによる、並進ベクトルと回転行列の計算を実施する関数
-    # data1  =  [x(t)1 x(t)2 x(t)3 ...]
-    # data2  =  [x(t + 1)1 x(t + 1)2 x(t + 1)3 ...]
-    # x = [x y z]'
-
+# ICPアルゴリズムによる、並進ベクトルと回転行列の計算を実施する関数
+# data1  =  [x(t)1 x(t)2 x(t)3 ...]
+# data2  =  [x(t + 1)1 x(t + 1)2 x(t + 1)3 ...]
+# x = [x y z]'
+def ICPMatching(data1, data2 , boundaryList = [[],[]] , gradientList = [[],[]]):
     #ICP パラメータ
     preError = 0    #一つ前のイタレーションのerror値
     dError = 1000   #エラー値の差分
@@ -85,7 +85,7 @@ def ICPMatching(data1, data2 , boundaryList = [[],[]]):
     #while False:
         count = count + 1
     
-        ii, error = FindNearestPoint(data1, data2, boundaryList)  #最近傍点探索
+        ii, error = FindNearestPoint(data1, data2, boundaryList,gradientList)  #最近傍点探索
         R1, t1 = SVDMotionEstimation(data1, data2, ii)    #特異値分解による移動量推定
 
         #計算したRとtで点群とRとtの値を更新
@@ -106,17 +106,12 @@ def ICPMatching(data1, data2 , boundaryList = [[],[]]):
     
     return R , t , data2 , preError
 
-def FindNearestPoint(data1, data2 , boundaryList = [[],[]]):
-    #data2に対するdata1の最近傍点のインデックスを計算する関数
+#data2に対するdata1の最近傍点のインデックスを計算する関数
+def FindNearestPoint(data1, data2 , boundaryList = [[],[]],gradientList = [[],[]]):
     m1 = data1.shape[1]
     m2 = data2.shape[1]
     index = [[],[]]
     error = 0
-
-    min = 100
-    max = 0
-    sum = 0.0
-    distList = []
 
     for i in range(0,m1,int(m1/200.0)):
         dx = data2 - np.matlib.repmat( transposition( data1[:,i] ), 1, m2 )
@@ -124,12 +119,12 @@ def FindNearestPoint(data1, data2 , boundaryList = [[],[]]):
 
         ii = np.argmin(dist)
         dist = np.min(dist)
-        
-        if min > dist:min = dist
-        if max < dist:max = dist
-        sum += dist
-        distList.append(dist)
 
+        #勾配を考慮
+        if len(gradientList[0]) != 0 and gradientList[0][i] > 0 and gradientList[1][ii] > 0:
+            dist *= abs(gradientList[0][i] - gradientList[1][ii])
+
+        #点の端(境界)を無視および一定以上距離の離れている点を除去
         if (len(boundaryList[0]) != 0 and ii in boundaryList[1]) or dist > 0.1:
             continue
 
@@ -137,14 +132,10 @@ def FindNearestPoint(data1, data2 , boundaryList = [[],[]]):
         index[1].append(ii)
         error = error + dist
 
-    #plt.hist(distList,bins = 50)
-    #plt.show()
-    #print("min:{},max:{},ave:{}".format(min,max,sum/m1))
     return index , error
 
+#特異値分解法による並進ベクトルと、回転行列の計算
 def SVDMotionEstimation(data1, data2, index):
-    #特異値分解法による並進ベクトルと、回転行列の計算
-
     #print("data size:{}=>{}".format(len(data1[0]),len(index[0])))
     #各点群の重心の計算
     M = data1[:,index[0]]
@@ -166,8 +157,8 @@ def SVDMotionEstimation(data1, data2, index):
 
     return R , t
 
+# degree to radian
 def toRadian(degree):
-    # degree to radian
     return degree / 180 * pi
 
 # arrayを転置する
@@ -178,6 +169,7 @@ def transposition(array):
     else:
         return array.T
 
+#指定したディレクトリ内のPCDファイルを取得
 def getPcdList(dirPath):
     pcdList = []
     count = 0
@@ -193,10 +185,11 @@ def getPcdList(dirPath):
 
     return sortedPcdList
 
+#PCDファイルから点群データを読み込み
 def getPointCloudData(filePath):
     pcd = open( DIRPATH + filePath , "r" )
 
-    retData = [[],[]]
+    retData = [[],[]]   #x,y
     boundaryData = []
     boundaryFlag = False #ゴミ値がfalse
     datacount = 0
@@ -207,6 +200,7 @@ def getPointCloudData(filePath):
         
         data = line.split(",")
 
+        #ゴミ値
         if float(data[0]) == 0.0 and float( data[1]) == 0.0:
             if boundaryFlag:
                 boundaryFlag = not boundaryFlag
@@ -226,8 +220,8 @@ def getPointCloudData(filePath):
         datacount += 1
 
         if not boundaryFlag:
-                boundaryFlag = not boundaryFlag
-                boundaryData.append(len(retData[0])-1)
+            boundaryFlag = not boundaryFlag
+            boundaryData.append(len(retData[0])-1)
 
     if boundaryFlag:
         boundaryFlag = not boundaryFlag
@@ -236,9 +230,30 @@ def getPointCloudData(filePath):
         del retData[0][-datacount:]
         del retData[1][-datacount:]
         del boundaryData[-2:]
-    print(len(retData[0]))
-    return retData , boundaryData
 
+    return retData , boundaryData , getGradient(retData,boundaryData)
+
+#各点の勾配リストを作成
+def getGradient(dataList , boundaryList):
+    gradientList = []
+    for index in range(len(dataList[0])):
+        if index in boundaryList:
+            gradientList.append(-1.0)
+        else:
+            gradientList.append(calcGradient(
+                [dataList[0][index]-dataList[0][index-1],dataList[1][index]-dataList[1][index-1]],
+                [dataList[0][index+1]-dataList[0][index],dataList[1][index+1]-dataList[1][index]]
+                ))
+    return gradientList
+
+#勾配の計算
+#勾配っていうか三点のなす角？
+def calcGradient(vector1,vector2):
+    det = vector1[0] * vector2[1] - vector1[1] * vector2[0]
+    inner = vector1[0] * vector2[0] + vector1[1] * vector2[1]
+    return atan2(det,inner)
+
+#データサイズを合わせる(致命的なバグあり)
 def adjustDataSize( data1 , data2 , max = 0):
     data1_length = len(data1[0]) 
     data2_length = len(data2[0])
@@ -263,6 +278,7 @@ def adjustDataSize( data1 , data2 , max = 0):
         else:
             return data1 , [random.sample(data2[0],data1_length),random.sample(data2[1],data1_length)]
 
+#点群データから画像作成
 def plotPoint2Image(data,img):
     imgMap = img.load()
     coefficient = 100.0 / 5.0
@@ -277,69 +293,55 @@ def plotPoint2Image(data,img):
            imgMap[x,y] += 50
     return img
 
+#PCDファイルからICPのテスト
 def pcdICPsample(minIndex = 0,maxIndex = 0):
     print (DIRPATH)
     pathList = getPcdList(DIRPATH)
 
-    if maxIndex == 0:
+    if maxIndex == 0 or maxIndex > len(pathList):
         maxIndex = len(pathList)
 
     print("Index : {} ~ {} \n\n".format(minIndex,maxIndex))
 
+    #変数宣言および初期化
     boundaryList = [[],[]]
-
-    preData, boundaryList[0] = getPointCloudData( pathList[minIndex] )
+    gradientList = [[],[]]
     preR = np.identity(2)
     preT = np.zeros([2,1])
-
-    #plt.grid(True)
-    #fig, ax = plt.subplots(1, 1)
-    #fig = plt.figure()
-    #ax = fig.add_subplot(1,1,1)
-
-    #lines, = ax.plot(preData[0,:],preData[1,:],marker = "o",color = "b",s = 10)
-
     img = Image.new("L",(400,400))
+
+    #初期データ読み込み
+    preData, boundaryList[0] , gradientList[0] = getPointCloudData( pathList[minIndex] )
 
     #for index, path in enumerate(pathList):
     for index in range(minIndex + 1,maxIndex,1):
         print ("\nindex:",index)
 
-        #data = getPointCloudData( path )
-        data, boundaryList[1] = getPointCloudData( pathList[index] )
+        #ファイルから点群の読み込み
+        data, boundaryList[1], gradientList[1] = getPointCloudData( pathList[index] )
         print(boundaryList)
+
+        #初期位置合わせ
         data = preR.dot( data )
         data = np.array([data[0] + preT[0],
                           data[1] + preT[1]])
 
+        #ICPMatchingがstr型("Max Iteration")を返す場合があるためtryで囲む
         try:
-            #boundaryList = [[],[]]
-            R,T,matchData,error = ICPMatching(np.array(preData),np.array(data),boundaryList)
+            #boundaryList = [[],[]] #有効にすると境界を無視しない
+            #gradientList = [[],[]] #有効にすると勾配を無視
+            R,T,matchData,error = ICPMatching(np.array(preData),np.array(data),boundaryList,gradientList)
 
         except TypeError:
             print("error")
             continue
         else:
-            #data1 , data2 = adjustDataSize(preData, data)
-            #print("Adjusted data size:{},{}".format(len(data1[0]),len(data1[0])))
-            #R,T,matchData = ICPMatching(np.array(data2),np.array(data1))
-
-            #ax.scatter(matchData[0,:],matchData[1,:],marker = "o",color = "b",s = 10)
-            
-            #plt.scatter(matchData[0,:],matchData[1,:],marker = "o",color = "b",s = 10)
-            #lines.set_data(matchData[0,:],matchData[1,:],marker = "o",color = "b",s = 10)
-            #ax.set_xlim((x.min(), x.max()))
-            #ax.set_ylim((y.min(), y.max()))
-            #plt.pause(.01)
-
-            #matchData = preR.dot( matchData )
-            #matchData = np.array([matchData[0] + preT[0],
-            #                      matchData[1] + preT[1]])
+            #もろもろ更新
             preR = R.dot( preR )
             preT = R.dot( preT ) + T 
             boundaryList[0] = boundaryList[1]
-
-            plt.scatter(matchData[0,:],matchData[1,:],marker = "o",color = "b",s = 10)
+            gradientList[0] = gradientList[1]
+            preData = matchData
 
             print('Estimated Motion [m m deg]:')
             theta  =  acos(R[0,0]) / pi * 180
@@ -348,20 +350,19 @@ def pcdICPsample(minIndex = 0,maxIndex = 0):
             print("Error:",error)
 
         img = plotPoint2Image(matchData,img)
-        ##img.show()
-        ##画像をarrayに変換
-        #im_list = np.asarray(img)
-        ##貼り付け
-        #plt.imshow(im_list)
-        #plt.gray()
-        ##表示
-        #plt.pause(.01)
 
+        #画像をarrayに変換
+        im_list = np.asarray(img)
+        #貼り付け
+        plt.imshow(im_list)
+        plt.gray()
+        #表示
+        plt.pause(.01)
+
+        ###１周分の点を表示
         #img2 = Image.new("L",(500,500))
         #img2 = plotPoint2Image(data,img2)
         #img2.show()
-
-        preData = matchData
 
     img.show()
 
@@ -370,10 +371,10 @@ def pcdICPsample(minIndex = 0,maxIndex = 0):
     #貼り付け
     plt.imshow(im_list)
     plt.gray()
-    #表示
+    #結果表示
     #plt.show()
 
 if __name__ == "__main__":
     #ICPsample()
     pcdICPsample(  )
-    #for i in range(11,20): pcdICPsample(i,i+3)
+    #for i in range(0,10): pcdICPsample(i,i+3)
